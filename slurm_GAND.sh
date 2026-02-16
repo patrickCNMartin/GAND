2a017c5e987a2e752e0dffa8bdcc5bef9b316048
patrickCNMartin <pcnmartin@gmail.com>
2026-02-16 10:58:03 -0800

    update config

--- slurm_GAND.sh
+++ slurm_GAND.sh
@@ -4,12 +4,12 @@
 #SBATCH --error=logs/%x-%j.err
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1G
+#SBATCH --mem=15G
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --mail-user=<patrick.martin@cshs.org>
-
+module load apptainer
 module load nextflow
-nextflow run main.nf -profile hpc
+nextflow run main.nf -profile container_hpc
